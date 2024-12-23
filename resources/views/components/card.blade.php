980d2ed7a700f5f4188c4540c28c5e844373ed29
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-12-23 13:49:31 +0100

    Build card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,17 +1,16 @@
-<div class="w-full bg-white rounded-lg shadow-lg p-4">
-    <div class="flex justify-between items-center">
-        <img src="" class="w-16 h-16 rounded-full mr-4" alt="">
-
-        <div>
-            <h2 class="text-lg font-semibold">product name</h2>
-            <p class="text-gray-600">product category</p>
+<div class="items-center flex h-fit w-full justify-center my-10">
+    <div class="items-center flex max-h-fit w-80 flex-col rounded-lg bg-gray-100 shadow-md">
+        <div class="h-72 w-3/4 rounded-md mb-3 flex items-center justify-center">
+            <img src="{{ asset('images/shirt.png') }}" alt="">
         </div>
+        <div class="mt-3 flex w-full flex-col items-start justify-start px-5">
+            <h1 class="text-left text-2xl font-extrabold uppercase text-black">Nike air jordan</h1>
+            <h3 class="text-lg font-medium text-gray-700">233 USD</h3>
         </div>
-        <div class="mt-4">
-        <p class="text-gray-800"> product</p>
-        <p class="text-xl font-bold mt-2">product</p>
-        </div>
-        <div class="mt-4 flex justify-end">
-        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Add to Cart</button>
-        </div>
+        <button
+            class="mt-2 rounded-full bg-blue-600 px-12 py-3 font-bold uppercase text-gray-200 hover:bg-blue-500 my-6">
+            Buy Now
+        </button>
+    </div>
 </div>
+
