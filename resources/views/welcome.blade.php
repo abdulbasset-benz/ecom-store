c931463a16e50df63777e708c1bd864c5cae6b76
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-12-25 20:17:27 +0100

    Change css for responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         class="bg-gray-200 flex flex-col md:flex-row items-center text-center w-screen overflow-hidden antialiased md:px-12">
         <div class="w-2xl md:max-w-2xl mx-auto px-12 pt-10 ">
             <div class="space-y-8">
-                <h1 class="text-5xl text-left font-alfa text-slate-900 leading-tight uppercase">
+                <h1 class="text-5xl md:text-6xl text-left font-alfa text-slate-900 leading-tight uppercase">
                     Find clothes that match your style
                 </h1>
                 <p class="text-gray-700 text-left max-w-sm">
@@ -42,63 +42,93 @@
                 <div class="flex justify-center items-center gap-5 flex-wrap pb-5">
                     <div>
                         <h2 class="text-2xl text-left md:text-4xl font-bold text-black">200+</h2>
-                        <p class="text-gray-500">International Brands</p>
+                        <p class="text-gray-500 text-left">International Brands</p>
                     </div>
                     <hr class="border border-gray-500 h-14">
                     <div>
                         <h2 class="text-2xl text-left font-bold md:text-4xl text-black">2,000+</h2>
-                        <p class="text-gray-500">High-Quality Products</p>
+                        <p class="text-gray-500 text-left">High-Quality Products</p>
                     </div>
                     <hr class="border opacity-0 md:opacity-100 border-gray-500 h-14">
                     <div>
                         <h2 class="text-2xl text-left font-bold md:text-4xl text-black">30,000+</h2>
-                        <p class="text-gray-500">Happy Customers</p>
+                        <p class="text-gray-500 text-left">Happy Customers</p>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="w-4xl h-auto mx-auto mt-4 relative">
-            <svg width="80px" height="80px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000"
-                class="bi bi-stars absolute right-10 top-0">
-                <path
-                    d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z" />
-            </svg>
+            <img src="images/icons/stars.svg" alt="stars" class="absolute top-20 right-0 w-24">
             <img src="{{ asset('images/fashion-man.png') }}" alt="Fashion Model" class="w-full h-auto max-w-lg">
-            <svg width="80px" height="80px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000"
-                class="bi bi-stars absolute left-10 bottom-5">
-                <path
-                    d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z" />
-            </svg>
+            <img src="images/icons/stars.svg" alt="" class="absolute bottom-20 left-0 w-24">
         </div>
     </main>
 
     {{-- brands section --}}
-    <section class="flex overflow-hidden">
-        <div class="flex py-3 bg-black w-screen">
+    <section class="flex overflow-hidden w-screen">
+        <div class="flex py-3 bg-black">
             <div class="flex gap-20 animate-loop-scroll px-5">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}" alt="Puma">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}" alt="Zara">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}"
+                    alt="Puma">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}"
+                    alt="Zara">
             </div>
             <div class="flex gap-20 animate-loop-scroll px-5">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}" alt="Puma">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}" alt="Zara">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}"
+                    alt="Puma">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}"
+                    alt="Zara">
             </div>
             <div class="flex gap-20 animate-loop-scroll px-5">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}" alt="Puma">
-                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}" alt="Zara">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/adidas.svg') }}" alt="Adidas">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/brand-nike.svg') }}" alt="Nike">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0"
+                    src="{{ asset('images/icons/calvin.svg') }}" alt="Calvin Klein">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/puma.svg') }}"
+                    alt="Puma">
+                <img class="w-24 mx-4 max-w-none filter invert brightness-0" src="{{ asset('images/icons/zara.svg') }}"
+                    alt="Zara">
             </div>
     </section>
 
+    {{-- cards --}}
+
+    <div class="flex justify-center flex-col items-center">
+        <h1 class="text-3xl md:text-7xl text-center font-black uppercase py-3">new arrivals</h1>
+    </div>
+
+    {{-- cards --}}
+    <div class="flex justify-start md:justify-center">
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+
+    </div>
+    <div class="flex items-center justify-center pt-5">
+        <button
+            class="px-24 md:px-16 py-3 my-4 text-slate-900 border border-slate-600 font-semibold rounded-full hover:bg-slate-900 hover:text-slate-50 transition">
+            Shop Now
+        </button>
+    </div>
+
+    
+    {{-- footer --}}
 
 
 
