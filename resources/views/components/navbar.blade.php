d0928fb17a0c8d7b3122fcbd9c61ad8c467f7f0e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-10 19:30:00 +0100

    Add animation to hamburger menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,12 @@
 <nav class="bg-white shadow-md w-screen p-4 md:p-6 sticky top-0 z-[100]">
-    <div class="container mx-auto px-5 flex items-center justify-between relative">
+    <div class="container mx-auto px-5 flex items-center justify-between flex-wrap relative">
         <!-- Hamburger Menu & Brand -->
-        <button class="lg:hidden pr-5 relative z-[102]" onclick="toggleNavLinks()">
-            <x-css-menu class="w-7 h-7 text-gray-600 hover:text-gray-900 cursor-pointer" />
+        <button class="group lg:hidden pr-5 relative z-[102]" onclick="toggleNavLinks()">
+            <div class="flex flex-col gap-2">
+                <span class="h-1 w-8 rounded-full bg-black group-hover:rotate-45 transition group-hover:translate-y-3"></span>
+                <span class="h-1 w-8 rounded-full bg-black group-hover:scale-0 transition"></span>
+                <span class="h-1 w-8 rounded-full bg-black group-hover:-rotate-45 transition group-hover:-translate-y-3"></span>
+            </div>
         </button>
         <div class="flex items-center space-x-4">
             <a href="#">
@@ -13,7 +17,6 @@
         <!-- Navigation Links -->
         <div class="fixed inset-0 bg-black bg-opacity-30 hidden lg:flex lg:relative lg:bg-transparent z-[90]"
             id="nav-overlay" onclick="toggleNavLinks()"></div>
-
         <div class="fixed top-0 left-0 w-full h-1/3 shadow-md transform -translate-x-full transition-transform lg:h-auto lg:relative lg:translate-x-0 lg:shadow-none lg:w-auto lg:flex lg:items-center z-[101] bg-white"
             id="nav-links">
             <ul
@@ -23,8 +26,6 @@
                 <li><a href="#" class="hover:text-gray-900 text-lg block p-4 lg:p-0">Products</a></li>
                 <li><a href="#" class="hover:text-gray-900 text-lg block p-4 lg:p-0">Contact</a></li>
             </ul>
-        </div>
-
         <!-- Search, Cart, and Profile -->
         <div class="flex items-center space-x-5">
             <div class="relative">
@@ -36,22 +37,24 @@
                     <input type="text" placeholder="Search"
                         class="bg-transparent outline-none text-gray-600 placeholder-gray-400 w-40 lg:w-64" />
                 </div>
-            </div>
             <a href="#" class="w-6 h-6 md:w-8 md:h-8">
                 <x-css-shopping-cart class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
-            </a>
-            <a href="#" class="w-6 h-6 md:w-8 md:h-8">
                 <x-css-profile class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
-            </a>
-        </div>
     </div>
 </nav>
 <script>
+    const span = document.querySelectorAll('span');
+
+    span.forEach((el) => {
+        el.addEventListener('click', () => {
+            el.classList.toggle('bg-green-900');
+        });
+    });
     function toggleNavLinks() {
         const navLinks = document.getElementById('nav-links');
         const navOverlay = document.getElementById('nav-overlay');
-
         navLinks.classList.toggle('-translate-x-full');
         navOverlay.classList.toggle('hidden');
     }
+
 </script>
