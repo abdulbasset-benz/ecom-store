f428fef96bc8da1de2a0cfa88bbb1aedd536e990
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-15 16:17:53 +0100

    Bug fixes : side menu and search bar toggling issues

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -19,7 +19,7 @@
             onclick="toggleNavLinks()"></div>
         <div class="fixed top-0 left-0 w-full h-auto bg-white transform -translate-x-full transition-transform lg:static lg:h-auto lg:translate-x-0 lg:bg-transparent lg:w-auto lg:flex lg:items-center z-[101]"
             id="nav-links">
-            <ul class="pt-20 px-5 lg:pt-0 lg:px-0 lg:flex lg:space-x-8 text-gray-600">
+            <ul class="pt-20 px-5 lg:pt-0 lg:px-0 lg:flex lg:space-x-8 text-gray-600 capitalize">
                 <li><a href="#" class="hover:text-gray-900 text-lg block p-4 lg:p-0">Home</a></li>
                 <li><a href="#" class="hover:text-gray-900 text-lg block p-4 lg:p-0">New Arrivals</a></li>
                 <li><a href="#" class="hover:text-gray-900 text-lg block p-4 lg:p-0">Products</a></li>
@@ -27,39 +27,35 @@
             </ul>
         </div>
 
+
         <!-- Right section: Search, Cart, and Profile -->
         <div class="flex items-center space-x-4">
-
-
             <!-- Search -->
 
+
             <div class="relative flex items-center">
-                <button class="search flex items-center">
-                    <x-css-search class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
-                </button>
                 <div
                     class="search-container fixed inset-0 h-14 bg-white p-4 translate-x-full transition-transform duration-300 z-[200] lg:relative lg:inset-auto lg:p-0 lg:transform-none flex items-center">
-                    <input type="search" class="w-full bg-gray-100 border border-gray-200 rounded-full p-2"
+                    <input type="search" class="w-full px-6 py-2 bg-gray-100 border border-gray-200 rounded-full "
                         placeholder="Search">
-                        <x-feathericon-x class="cursor-pointer cursor-icon"/>
-
+                    <x-feathericon-x class="w-7 h-7 cursor-pointer cursor-icon md:hidden" />
                 </div>
             </div>
 
-
-
-
-
             <!-- Cart and Profile -->
-            <div class="flex items-center space-x-4">
-                <a href="#" class="block">
-                    <x-css-shopping-cart class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
-                </a>
-                <a href="#" class="block">
-                    <x-css-profile class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
-                </a>
-            </div>
+            <button class="search">
+                <x-css-search class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
+            </button>
+
+            <a href="/cart-index" class="block">
+                <x-css-shopping-cart class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
+            </a>
+            <a href="#" class="block">
+                <x-css-profile class="w-7 h-7 text-gray-600 hover:text-gray-900 md:w-8 md:h-8" />
+            </a>
         </div>
+
+    </div>
     </div>
 </nav>
 
