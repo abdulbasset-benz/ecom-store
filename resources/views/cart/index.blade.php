05a49c177e0edfc43b14732cdcd0a62b1a2eb95b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-17 20:53:48 +0100

    Add order summary section for mobile

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -13,7 +13,7 @@
                         <h2 class="text-2xl font-bold flex items-center gap-2">
                             Nike Sneakers cyan
                             <span class="inline-block cursor-pointer text-center">
-                                <x-css-trash class="text-red-600"/>
+                                <x-css-trash class="text-red-600" />
                             </span>
                         </h2>
 
@@ -29,9 +29,9 @@
                         <div class="flex items-center justify-between gap-4">
                             <p class="text-2xl font-bold text-gray-900 total"> $</p>
                             <span class="flex items-center gap-4 bg-gray-300 px-5 py-2 rounded-full">
-                                <x-css-math-minus class="cursor-pointer minus"/>
+                                <x-css-math-minus class="cursor-pointer minus" />
                                 <span class="quantity text-xl font-bold">1</span>
-                                <x-css-math-plus class="cursor-pointer plus"/>
+                                <x-css-math-plus class="cursor-pointer plus" />
                             </span>
                         </div>
                     </div>
@@ -44,6 +44,30 @@
         </div>
     </div>
 
+    {{-- order summary --}}
+    <div class="container mx-auto p-4 flex flex-col gap-5">
+        <div class="flex flex-col max-w-full border-2 border-gray-300 rounded-lg p-5 gap-4">
+            <h1 class="text-2xl capitalize font-bold">order summary</h1>
+            <div class="flex justify-between">
+                <p class="text-xl text-gray-400 capitalize">subtotal</p>
+                <span class="text-xl font-bold text-black">565 $</span>
+            </div>
+            <div class="flex justify-between">
+                <p class="text-xl text-red-600 capitalize">discount (20%)</p>
+                <span class="text-xl font-bold text-red-600">-113 $</span>
+            </div>
+            <div class="flex justify-between">
+                <p class="text-xl text-gray-400 capitalize">delivery fee</p>
+                <span class="text-xl font-bold text-black">15 $</span>
+            </div>
+            <hr class="border-gray-300">
+            <div class="flex justify-between">
+                <p class="text-xl text-gray-600 font-bold capitalize">total</p>
+                <span class="text-xl font-bold">467 $</span>
+            </div>
+            <button class="bg-black text-xl font-bold text-gray-200 capitalize py-5 rounded-full mt-5 hover:text-gray-100">Confirm your order</button>
+        </div>
+    </div>
     <script>
         const plusButtons = document.querySelectorAll('.plus');
         const minusButtons = document.querySelectorAll('.minus');
