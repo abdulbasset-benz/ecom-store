1e0207a4b8d38ad2a2ff0c1274894e375f69ffb0
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-17 19:11:58 +0100

    structuring the layout for customization

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto p-4">
+    <div class="bg-green-400 container mx-auto p-4 flex flex-col gap-5">
         <h1 class="uppercase text-3xl font-bold">your cart</h1>
-        <div class="flex w-full border border-gray-600 rounded-lg p-5">
-            <img src="{{ asset('images/sneakers.jpeg') }}" alt="" class="w-32 rounded-sm mr-5">
-            <div>
-                <h2 class="text-2xl font-bold">Nike Sneakers cyan color</h2>
-                <p class="text-lg">Price: $100</p>
-                <p class="text-sm">Quantity: 1</p>
-                <p class="text-sm">Total: $100</p>
-            </div>
+        <div class="flex max-w-lg border border-gray-600 rounded-lg p-5">
+            @for ($i = 0; $i < 3; $i++)
+                <img src="{{ asset('images/sneakers.jpeg') }}" alt="" class="w-32 rounded-sm mr-5">
+                <div class=" flex flex-col justify-between items-start py-5 gap-2 flex-nowrap">
+                    <h2 class="text-2xl font-bold">Nike Sneakers cyan color</h2>
+                    <p class="text-lg text-gray-900">Price: <span class="text-gray-500">100 $</span></p>
+                    <p class="text-lg text-gray-900">Quantity: <span class="text-gray-500">2</span></p>
+                    <p class="text-2xl font-bold text-gray-900">200 $</p>
+                </div>
+            @endfor
         </div>
     </div>
 @endsection
