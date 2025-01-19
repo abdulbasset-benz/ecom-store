ff9efe0239921faa0ee47de46fde7937ceb4bb6d
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-19 23:18:03 +0100

    Minor changes

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container p-5 mx-auto">
+        {{-- header section --}}
+        <div class="flex justify-between items-center">
+            <h1 class="text-2xl font-bold capitalize">casual</h1>
+            <span class="bg-gray-300 rounded-full p-2 cursor-pointer"><x-letsicon-filter class="w-7" />
+            </span>
+        </div>
+
+        {{-- products show section --}}
+        <div class="flex flex-wrap justify-center items-center gap-5 p-2">
+            @for ($i = 0; $i < 6; $i++)
+            <x-card />
+            @endfor
+        </div>
+    </div>
+@endsection
