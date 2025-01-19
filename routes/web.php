fe24c75953db5aee838ef53a630fe2fad8b8446f
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-19 20:46:20 +0100

    Add routing for products page, will be updated later

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/product-details', function () {
     return view('products.details');
 });
 
+Route::get('/products', function () {
+    return view('products.index');
+});
+
 Route::get('/cart-index', function () {
     return view('cart.index');
 });
