6fc051fcd6b19f449f418ec0cd0319e17b1acb6a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-20 23:15:14 +0100

    Minor change in the grids

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="container p-5 mx-auto">
-        {{-- header section --}}
+
         <div class="flex justify-between items-center">
             <h1 class="text-2xl font-bold capitalize">casual</h1>
-            <span class="bg-gray-300 rounded-full p-2 cursor-pointer"><x-letsicon-filter class="w-7" />
+            <span class="bg-gray-300 rounded-full p-2 cursor-pointer hover:bg-gray-400"><x-letsicon-filter class="w-7" />
             </span>
         </div>
 
         {{-- products show section --}}
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
             @for ($i = 0; $i < 6; $i++)
             <x-card />
             @endfor
