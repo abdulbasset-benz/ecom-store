6d2c093d5fcc266c25516707965c3167ebb88ef2
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-24 22:31:36 +0100

    Refactor code for desktop, add scrollbar, adjust filters icon

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="relative container p-5 mx-auto">
+    <div class="relative container mx-auto min-h-screen overflow-hidden lg:overflow-visible">
         {{-- Mobile Header - Only visible on small screens --}}
         <div class="flex justify-between items-center lg:hidden">
             <h1 class="text-2xl font-bold capitalize">casual</h1>
@@ -14,14 +14,16 @@
         <div class="lg:flex lg:gap-6">
 
             <div id="filters"
-                class="fixed inset-0 translate-x-full bg-slate-50 transition-transform duration-300 ease-in-out z-50 overflow-y-auto
-                lg:static lg:translate-x-0 lg:w-[300px] lg:min-h-screen lg:bg-transparent lg:shadow-none
-                [&::-webkit-scrollbar]:w-2
-                [&::-webkit-scrollbar-track]:bg-gray-200
-                [&::-webkit-scrollbar-thumb]:bg-gray-500
-                [&::-webkit-scrollbar-thumb]:rounded-full">
+                class="fixed inset-0 mt-16 translate-x-full bg-slate-50 transition-transform duration-300 ease-in-out z-50 overflow-y-auto
+    lg:static lg:translate-x-0 lg:w-[300px] lg:h-screen lg:border-2 lg:rounded-xl lg:bg-transparent lg:shadow-none
+    lg:overflow-y-auto
+    [&::-webkit-scrollbar]:w-1
+    [&::-webkit-scrollbar-track]:bg-gray-300
+    [&::-webkit-scrollbar-thumb]:bg-gray-500
+    [&::-webkit-scrollbar-thumb]:rounded-full">
 
-                <div class="pt-16 pb-16 px-10">
+
+                <div class="pt-16 pb-16 px-10 lg:bg-transparent lg:overflow-y-visible">
 
                     {{-- Mobile Filter Header --}}
                     <div class="flex justify-between items-center lg:hidden">
@@ -30,29 +32,39 @@
                     </div>
 
                     {{-- Desktop Filter Header --}}
-                    <h1 class="text-2xl font-bold hidden lg:block">Filters</h1>
+                    <div class="flex justify-between items-center lg:flex">
+                        <h1 class="text-2xl font-bold hidden lg:block">Filters</h1>
+                        <span class=" cursor-pointer rounded-full bg-slate-300 p-2"><x-letsicon-filter
+                                class="w-7" /></span>
+                    </div>
+
                     <hr class="my-3 bg-slate-400">
 
                     {{-- Categories Section --}}
                     <div class="space-y-3 px-5">
                         <ul class="flex flex-col gap-4">
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Tshirts
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Jackets
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Shirts
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Hoodies
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Jeans
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
@@ -66,7 +78,8 @@
                         <span><x-eva-arrow-ios-forward-outline class="w-6 h-6" /></span>
                     </div>
                     <div class="w-[90%] mx-auto">
-                        <input class="w-full accent-black p-4" type="range" min="0" max="500" step="10" value="250">
+                        <input class="w-full accent-black p-4" type="range" min="0" max="500" step="10"
+                            value="250">
 
                     </div>
 
@@ -76,7 +89,7 @@
                         <h1 class="text-2xl font-bold">Sizes</h1>
                         <span><x-eva-arrow-ios-forward-outline class="w-6 h-6" /></span>
                     </div>
-                    <div class="flex flex-wrap gap-2 items-center p-8">
+                    <div class="flex flex-wrap  gap-2 items-center p-8">
                         <h3
                             class="text-gray-500 text-lg bg-slate-200 px-6 py-2 rounded-full cursor-pointer
                             hover:bg-gray-900 hover:text-slate-100">
@@ -111,23 +124,28 @@
                     </div>
                     <div class="space-y-3 pt-5 px-5">
                         <ul class="flex flex-col gap-4">
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Casual
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 StreetWear
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Formal
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Party
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
-                            <li class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
+                            <li
+                                class="flex items-center justify-between text-xl text-gray-500 group hover:text-gray-700 cursor-pointer">
                                 Gym
                                 <x-eva-arrow-ios-forward-outline class="w-6 h-6 ml-2 group-hover:text-gray-700" />
                             </li>
@@ -136,8 +154,9 @@
 
                     {{-- Apply Filters Button - Only visible on mobile --}}
                     <hr class="my-3 bg-slate-400">
-                    <div class="flex flex-col gap-5 mx-auto w-2/3 lg:hidden">
-                        <button aria-label="Apply filters" class="bg-black text-xl font-bold text-gray-200 capitalize py-3 px-8 rounded-full mt-5 hover:text-gray-100">
+                    <div class="flex flex-col gap-5 mx-auto w-3/3">
+                        <button aria-label="Apply filters"
+                            class="bg-black text-xl font-bold text-gray-200 capitalize py-3 px-8 rounded-full mt-5 hover:text-gray-100">
                             Apply filters
                         </button>
 
