6a101030b0e566c4c6f13b6fac66ddbcedbf5b1c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-01-31 21:41:23 +0100

    Add responsive design to images and on focus effect

--- resources/views/products/details.blade.php
+++ resources/views/products/details.blade.php
@@ -1,17 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="bg-slate-300 w-full h-1/2 p-5 rounded-lg flex items-center justify-center flex-col">
-        <div class="w-full h-1/2 bg-slate-100 rounded-lg m-2">
-            <img class="h-full w-full" src="{{ asset('images/shirt.png') }}" alt="">
+    <div class="container grid grid-cols-1 p-4">
+
+        {{-- images layout --}}
+        <div class="max-w-sm mx-auto">
+            <img tabindex="0" class="w-full rounded-lg focus:ring-4 focus:ring-pink-500 focus:outline-none"
+                src="{{ asset('images/black.jpeg') }}" alt="">
+            <div class="grid grid-cols-4 gap-4 mt-4 px-1">
+                <img tabindex="0"
+                    class="w-full h-auto object-fill rounded-lg focus:ring-2 focus:ring-pink-500 focus:outline-none"
+                    src="{{ asset('images/shoe1.jpg') }}" alt="">
+                <img tabindex="0"
+                    class="w-full h-auto object-fill rounded-lg focus:ring-2 focus:ring-pink-500 focus:outline-none"
+                    src="{{ asset('images/shoe4.jpg') }}" alt="">
+                <img tabindex="0"
+                    class="w-full h-auto object-fill rounded-lg focus:ring-2 focus:ring-pink-500 focus:outline-none"
+                    src="{{ asset('images/green.jpeg') }}" alt="">
+                <img tabindex="0"
+                    class="w-full h-auto object-fill rounded-lg focus:ring-2 focus:ring-pink-500 focus:outline-none"
+                    src="{{ asset('images/shoe2.jpg') }}" alt="">
+            </div>
+
+
+            {{-- product details --}}
         </div>
 
-        <div class="flex justify-between items-center w-full gap-2">
-            <img class="size-32 rounded-md cursor-pointer " src="{{ asset('images/casual1.jpg') }}" alt="">
-            <img class="size-32 rounded-md cursor-pointer " src="{{ asset('images/black.jpeg') }}" alt="">
-            <img class="size-32 rounded-md cursor-pointer " src="{{ asset('images/formal.jpeg') }}" alt="">
-        </div>
     </div>
+
 
     <script></script>
 @endsection
