07ef7d4cd2f7de8a3b4fd105c65494c1409bd379
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-04 22:37:40 +0100

    Fix quantity button and code refactor

--- resources/views/products/details.blade.php
+++ resources/views/products/details.blade.php
@@ -53,11 +53,16 @@
             {{-- Quantity & Add to Cart --}}
             <div class="grid grid-cols-3 gap-4 mt-6 items-center">
 
-                <div class="flex items-center justify-between gap-2 bg-gray-300 col-span-1 py-3 rounded-full">
-                    <x-css-math-minus class="cursor-pointer hover:text-red-500 minus" />
+                <div class="flex items-center justify-between bg-gray-300 col-span-1 rounded-full">
+                    <button class=" group px-9 py-2 rounded-l-full hover:bg-red-300">
+                        <span class="font-bold text-2xl scale-200 group-hover:text-red-500">-</span>
+                    </button>
                     <span class="quantity text-xl font-bold">1</span>
-                    <x-css-math-plus class="cursor-pointer hover:text-green-500 plus" />
+                    <button class=" group px-9 py-2 rounded-r-full hover:bg-green-300">
+                        <span class="font-bold text-2xl scale-200 group-hover:text-green-500">+</span>
+                    </button>
                 </div>
+
 
                 <!-- Add to Cart Button -->
                 <button
