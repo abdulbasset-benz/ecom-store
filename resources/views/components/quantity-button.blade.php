4957daafb499835883555748d22f7e3f1e03cb81
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-04 23:03:14 +0100

    Create qauntity btn view

---
+++ resources/views/components/quantity-button.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center justify-between bg-gray-300 col-span-1 rounded-full">
+    <button class=" group px-9 py-2 rounded-l-full hover:bg-red-300">
+        <span class="font-bold text-2xl scale-200 group-hover:text-red-500">-</span>
+    </button>
+    <span class="quantity text-xl font-bold">1</span>
+    <button class=" group px-9 py-2 rounded-r-full hover:bg-green-300">
+        <span class="font-bold text-2xl scale-200 group-hover:text-green-500">+</span>
+    </button>
+</div>
