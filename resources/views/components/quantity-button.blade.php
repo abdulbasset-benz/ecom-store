8b41702b06a7032d8607372db75988786d34d847
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-09 22:33:45 +0100

    Minor changes

--- resources/views/components/quantity-button.blade.php
+++ resources/views/components/quantity-button.blade.php
@@ -1,11 +1,11 @@
 <div class="flex items-center justify-between bg-gray-300 rounded-full">
-    <button class=" group px-6 py-3 rounded-l-full hover:bg-red-300">
-        <x-feathericon-minus class="group-hover:text-red-700" />
+    <button class=" group px-6 py-3 rounded-l-full">
+        <x-feathericon-minus class="group-hover:text-red-500" />
 
     </button>
     <span class="quantity font-bold text-xl px-2">1</span>
-    <button class=" group px-6 py-3 rounded-r-full hover:bg-green-300">
-        <x-feathericon-plus class="group-hover:text-green-700"/>
+    <button class=" group px-6 py-3 rounded-r-full">
+        <x-feathericon-plus class="group-hover:text-green-500"/>
 
     </button>
 </div>
