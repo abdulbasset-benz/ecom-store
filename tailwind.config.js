d0dc67cb07ab7785e9c9c12c7c48c146b311e851
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-10 22:10:09 +0100

    Fix bug with font being deleted after installing breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,16 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                alfa: ['Alfa Slab One', ...defaultTheme.fontFamily.sans],
             },
         },
     },
