a442355fbbf2bdd1d260881eca41a15616e506e7
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-11 22:12:07 +0100

    Modify routes for cart and contact page

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
 
 Route::get('/product-details', function () {
     return view('products.details');
 });
 
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('/cart-index', function () {
+    return view('cart.index');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
