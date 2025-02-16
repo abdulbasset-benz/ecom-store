9fa4dd65f8b7c95492c42a014c9ef2d4fb4c4151
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-16 23:39:57 +0100

    Refactor to achive the right positioning for newsletter section

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,22 +1,27 @@
-<div class="bg-slate-200 relative h-full mt-72 flex justify-center items-center p-4">
-    <div class="bg-black hidden absolute bottom-8 rounded-3xl flex flex-col items-start justify-center p-8 gap-4">
-        <h1 class="text-4xl md:text-6xl font-alfa text-white text-left uppercase px-5">
+<div class="bg-gray-200 relative min-h-[500px] mt-48 p-4">
+    <div class="bg-black w-[90%] mx-auto absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl p-8 gap-4">
+        <h1 class="text-4xl md:text-6xl font-alfa text-white text-left uppercase px-1">
             stay up to date with our latest offers
         </h1>
-        <form action="" class="flex flex-col items-start justify-center gap-4 w-full">
+        <form action="" class="flex flex-col gap-4 mt-4">
             @csrf
-            <input type="text" placeholder="Enter your email" class="border-none text-lg rounded-full p-3 w-full">
-            <button class="px-6 py-3 rounded-full bg-slate-50 text-slate-900 text-lg w-full">Subscribe to our
-                Newsletter</button>
+            <input type="text" placeholder="Enter your email"
+                   class="border-none text-lg rounded-full p-3 w-full">
+            <button class="px-6 py-3 rounded-full bg-slate-50 text-slate-900 text-lg w-full">
+                Subscribe to our Newsletter
+            </button>
         </form>
     </div>
 
-    <div class="flex flex-col items-start justify-center gap-2">
-        <h1 class="text-4xl md:text-6xl font-alfa text-white text-left uppercase">
-            SHOP.CO
-        </h1>
-        <p class="text-slate-500 text-left">
-            We have clothes that suits your style and which you're proud to wear. from men to women
-        </p>
+
+    <div class="pt-[38%] md:pt-[15%] w-[90%] mx-auto">
+        <div class="flex flex-col items-start justify-center gap-2 max-w-6xl mx-auto">
+            <h1 class="text-4xl md:text-6xl font-alfa text-black text-left uppercase">
+                SHOP.CO
+            </h1>
+            <p class="text-slate-500 text-left">
+                We have clothes that suits your style and which you're proud to wear. from men to women
+            </p>
+        </div>
     </div>
 </div>
