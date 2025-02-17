91c1015f2d4141fa57fd9940f855ab8307b90705
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-17 22:13:19 +0100

    Add letter icon to the input element

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,21 @@
 <div class="bg-gray-200 relative min-h-[500px] mt-48 p-4">
-    <div
-        class="bg-black w-[90%] mx-auto absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl p-8 gap-4">
+    <div class="bg-black w-[90%] mx-auto absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl p-8 gap-4">
         <h1 class="text-4xl md:text-6xl font-alfa text-white text-left uppercase px-1">
             stay up to date with our latest offers
         </h1>
         <form action="" class="flex flex-col gap-4 mt-4">
             @csrf
-            <input type="text" placeholder="Enter your email" class="border-none text-lg rounded-full p-3 w-full">
-            <button class="px-6 py-3 rounded-full bg-slate-50 text-slate-900 text-lg w-full">
+            <div class="relative w-full">
+                <x-bi-envelope class="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500"/>
+                <input type="text" placeholder="Enter your email"
+                    class="border-none text-lg rounded-full p-3 pl-12 w-full">
+            </div>
+            <button class="px-6 py-3 rounded-full bg-slate-50 text-slate-900 text-lg w-full hover:text-white hover:bg-black">
                 Subscribe to our Newsletter
             </button>
         </form>
     </div>
+
 
     <div class="pt-[38%] md:pt-[15%] w-[90%] mx-auto">
         <div class="flex flex-col items-start justify-center gap-4 max-w-6xl mx-auto">
