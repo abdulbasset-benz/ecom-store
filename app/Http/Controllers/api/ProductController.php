b4bb6fb1b3be897d5868725d52ca6f9cc2087344
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-24 23:13:53 +0100

    Add basic product management methods. will be updated later on

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use GuzzleHttp\Handler\Proxy;
 
 class ProductController extends Controller
 {
@@ -26,6 +27,7 @@ class ProductController extends Controller
             'name' => 'required|string|max:255',
             'description' => 'required|string',
             'price' => 'required|numeric',
+            'category' => 'nullable|string|max:255'
         ]);
 
         Product::create($data);
@@ -38,15 +40,33 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        if(!$product){
+            return response()->json(['message' => 'product not found dude']);
+        }
+
+        return response()->json($product);
     }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
+
     {
-        //
+        $product = Product::findOrFail($id);
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'category' => 'nullable|string|max:255'
+        ]);
+
+        $product->update($data);
+
+        return response()->json($product);
     }
 
     /**
@@ -54,6 +74,10 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return response()->json(['message' => 'product deleted ya kho']);
     }
 }
