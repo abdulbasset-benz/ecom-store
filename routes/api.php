e33bf7d4586f1a83602ed13be9a86a126415a0bd
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-01 15:37:34 +0100

    Add auth routes for registraton, login and logout

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\ProductController;
 
 // Auth Routes
-Route::post('/register', AuthController::class, 'register');
-Route::post('/login', AuthController::class, 'login');
-Route::post('/logout', AuthController::class, 'logout')->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
