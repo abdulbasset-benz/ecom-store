c57a250a4bbffed1212034bc368f0eaa96b3d33a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-01 14:49:04 +0100

    Configure laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Container\Attributes\Auth;
+use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\ProductController;
+
+// Auth Routes
+Route::post('/register', AuthController::class, 'register');
+Route::post('/login', AuthController::class, 'login');
+Route::post('/logout', AuthController::class, 'logout')->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
