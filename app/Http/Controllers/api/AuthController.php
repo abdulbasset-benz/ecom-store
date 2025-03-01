06145d3781673f30912e2a3fe2a30f4c5ae67386
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-01 18:08:10 +0100

    Add Login method and fix the bugs it had with createToken

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -32,4 +34,31 @@ class AuthController extends Controller
             'token_type' => 'Bearer'
         ]);
     }
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => Auth::user(),
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
 }
