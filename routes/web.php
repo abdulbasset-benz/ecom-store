0844107a3ee6ff08003380bc5a755177c6edf796
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-01 14:24:06 +0100

    Remove breeze since it is useless for api

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,3 @@ Route::get('/contact', function () {
 Route::get('/cart-index', function () {
     return view('cart.index');
 });
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
