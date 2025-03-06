7868746985423bbb28b5c101d7a32abb467a7d89
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-06 02:27:27 +0100

    Add attributes to the table

--- database/migrations/2025_03_03_231501_create_orders_table.php
+++ database/migrations/2025_03_03_231501_create_orders_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->decimal('total_amount', 8, 2);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
