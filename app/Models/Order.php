95818e6d14e7bc2a4cf1782c6ef974c2a1a93a5b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-06 02:23:36 +0100

    Add relationships to the Cart model and mass assignment

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'total_amount'
+    ];
+
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product() : BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'order_products')->withPivot('quantity', 'price');
+    }
+
+
 }
