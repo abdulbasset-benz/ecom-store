e9532b401b74e5c5288ff6ca2d82c86ac307eb92
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-06 02:22:56 +0100

    Add relationships to the product model and mass assignment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,13 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'description',
         'price',
         'category'
     ];
+
+    public function orders(){
+        return $this->belongsToMany(Order::class, 'order_products')->withPivot('quantity', 'price');
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
