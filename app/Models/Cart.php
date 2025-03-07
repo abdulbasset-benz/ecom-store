f9916a829a9cbed4fd988a6f008e50440f59e84c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-08 00:10:51 +0100

    setup relationships correctly

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,18 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Cart extends Model
 {
-    public function user()
+    protected $fillable = [
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function products()
+    public function products(): BelongsToMany
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsToMany(Product::class, 'cart_products')
+                    ->withPivot('quantity', 'price')
+                    ->withTimestamps();
     }
 
-    
+    public function getTotalAttribute(): float
+    {
+        return $this->products->sum(function ($product) {
+            return $product->pivot->quantity * $product->pivot->price;
+        });
+    }
 }
