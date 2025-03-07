6c8e6b21aba12e884b48a4311487af2059b76bc2
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-08 00:04:45 +0100

    Refactor code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -13,14 +15,26 @@ class Product extends Model
         'name',
         'description',
         'price',
-        'category'
+        'stock',
+        'category_id'
     ];
 
-    public function orders(){
-        return $this->belongsToMany(Order::class, 'order_products')->withPivot('quantity', 'price');
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class, 'order_products')
+                    ->withPivot('quantity', 'price')
+                    ->withTimestamps();
     }
 
-    public function category(){
+    public function category(): BelongsTo
+    {
         return $this->belongsTo(Category::class);
+    }
+
+    public function carts(): BelongsToMany
+    {
+        return $this->belongsToMany(Cart::class, 'cart_products')
+                    ->withPivot('quantity', 'price')
+                    ->withTimestamps();
     }
 }
