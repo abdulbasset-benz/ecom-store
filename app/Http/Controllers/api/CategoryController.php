c15ec8b07246c29b260f8b8799275cf5252ee33f
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-10 03:39:05 +0100

    feat: create CategoryController with full CRUD methods

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -3,9 +3,61 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::paginate(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+
+        $data = $request->validated();
+
+        if (Category::where('name', $data['name'])->exists()) {
+            return response()->json(['message' => 'Category already exists'], 409);
+        }
+        $category = Category::create($data);
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new CategoryResource(Category::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::FindOrFail($id);
+        $category->update($request->all());
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json(['message' => 'Category deleted ya kho']);
+    }
 }
