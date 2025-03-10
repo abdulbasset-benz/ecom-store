2b126c96e35071f53c09305268bc328569491698
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-10 03:45:08 +0100

    feat: add API routes for CategoryController

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Container\Attributes\Auth;
 use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\CategoryController;
 
 // Auth Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Category Routes
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
