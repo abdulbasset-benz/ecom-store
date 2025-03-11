aa5b2f5b999dfa6fd58e3e0d6d7b9a556f89c80d
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-03-11 04:55:47 +0100

    Fix : change namespace and controller naming to remove conflict with web productController routes

---
+++ app/Http/Controllers/Api/ApiProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+
+class ApiProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return ProductResource::collection(Product::paginate(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $data = $request->validated();
+        $product = Product::create($data);
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+
+        return new ProductResource(Product::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, string $id)
+
+    {
+        $product = Product::FindOrFail($id);
+        $product->update($request->validated());
+        return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->delete();
+
+        return response()->json(['message' => 'product deleted ya kho']);
+    }
+}
