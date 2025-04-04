5932155e3db62efe8e5e6438f8887059b1903161
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-04 12:53:40 +0100

    register the cors middelware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+        
         $middleware->group('api', [
             EnsureFrontendRequestsAreStateful::class,
         ]);
